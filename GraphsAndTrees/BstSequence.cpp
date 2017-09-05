f6650a05b2329f7da10368b39fc47fa9b8fc7b04
chivora <chivora@10.155.152.25>
2017-09-05 10:53:33 -0700

    Trees and Graph Problems

--- GraphsAndTrees/BstSequence.cpp
+++ GraphsAndTrees/BstSequence.cpp
@@ -10,7 +10,8 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<list>
-#include "require.h"
+#include <algorithm>    // std::next_permutation, std::sort
+#include <vector>
 
 using namespace std;
 
@@ -65,34 +66,155 @@ void printInorder (TNode* node)
     printf("%d ", node->data);
     printInorder(node->right);
 }
+/*
+ *
+I assume you want a list of all sequences which will generate the same BST.
+In this answer, we will use Divide and Conquer.
+We will create a function findAllSequences(Node *ptr) which takes a node pointer as input and returns all the distinct
+sequences which will generate the subtree hanging from ptr. This function will return a Vector of Vector of int, i.e.
+vector<vector<int>> containing all the sequences.
 
-std::list< std::list<int> > allSequences(TNode* node){
+The main idea for generating sequence is that root must come before all its children.
 
-	std::list< std::list<int> > result =
-			new std::list< std::list<int> >();
+Algorithm:
 
-	if(node == NULL){
-		result.insert(new std::list<int>());
-		return result;
-	}
+Base Case 1:
+If ptr is NULL, then return a vector with an empty sequence.
 
-	std::list<int>prefix = new std::list<int>();
-	prefix.insert(node->data);
+if (ptr == NULL) {
+    vector<int> seq;
+    vector<vector<int> > v;
+    v.push_back(seq);
+    return v;
+}
+Base Case 2:
+If ptr is a leaf node, then return a vector with a single sequence. Its Trivial that this sequence will contain only a single element,
+i.e. value of that node.
 
+if (ptr -> left == NULL && ptr -> right == NULL) {
+    vector<int> seq;
+    seq.push_back(ptr -> val);
+    vector<vector<int> > v;
+    v.push_back(seq);
+    return v;
+}
+
+Divide Part (this part is very simple.)
+We assume that we have a function that can solve this problem, and thus we solve it for left sub tree and right sub tree.
+
+vector<vector<int> > leftSeq  = findAllSeq(ptr -> left);
+vector<vector<int> > rightSeq = findAllSeq(ptr -> right);
+
+Merging the two solution.(The crux is in this step.)
+Till now we have two set containing distinct sequences:
+i. leftSeq  - all sequences in this set will generate left subtree.
+ii. rightSeq - all sequences in this set will generate right subtree.
+Now each sequence in left subtree can be merged with each sequence of right subtree. While merging we should be careful that the
+relative order of elements is preserved. Also in each of the merged sequence we will add the value of current node in the
+beginning because root must come before all children.
+
+Pseudocode for Merge
+
+vector<vector<int> > results
+for all sequences L in leftSeq
+    for all sequences R in rightSeq
+        create a vector flags with l.size() 0's and R.size() 1's
+        for all permutations of flag
+            generate the corresponding merged sequence.
+            append the current node's value in beginning
+            add this sequence to the results.
+return results.
+
+Explaination: Let us take a sequence, say L(of size n) from the set leftSeq, and a sequence, say R(of size m) from set rightSeq.
+Now these two sequences can be merged in m+nCn ways!
+Proof: After merging, the new sequence will have m + n elements. As we have to maintain the relative order of elements,
+so firstly we will fill all n the elements from L in any of n places among total (m+n) places.
+After that remaining m places can be filled by elements of R. Thus we have to choose n places from (m+n) places.
+To do this, lets create take a Boolean vector, say flags and fill it with n 0's and m 1's.
+A value of 0 represents a member from left sequence and a value of 1 represents member from  right sequence.
+All what is left is to generate all permutations of this flags vector, which can be done with next_permutation.
+Now for each permutation of flags we will have a distinct merged sequence of L and R.
+eg: Say L={1, 2, 3} R={4, 5}
+so, n=3 and m=2
+thus, we can have 3+2C3 merged sequences, i.e. 10.
+1.now, Initially flags = {0 0 0 1 1}, filled with 3 0's and 2 1's
+this will result into this merged sequence: 1 2 3 4 5
+2.after calling nextPermutation we will have
+flags = {0 0 1 0 1}
+and this will generate sequence: 1 2 4 3 5
+3.again after calling nextPermutation we will have
+flags = {0 0 1 1 0}
+ans this will generate sequence: 1 2 4 5 3
+and so on...
+ */
+
+
+vector<vector<int> > findAllSeq(TNode *ptr)
+{
 	/*
-	 * Recurse on left and right subtree
+	 * Base Case 1:
+	 * If ptr is NULL, then return a vector with an empty sequence.
 	 */
-	std::list< std::list<int> > leftSeq =
-			new std::list< std::list<int> >();
-	std::list< std::list<int> > rightSeq =
-			new std::list< std::list<int> >();
+    if (ptr == NULL) {
+        vector<int> seq;
+        vector<vector<int> > v;
+        v.push_back(seq);
+        return v;
+    }
 
-	/*
-	 * Weave together each list from left and right side
-	 */
-	for(int i=0;i<leftSeq.size();i++){
+    /*
+     * Base Case 2:
+     * ptr is a leaf node, then return a vector with a single sequence.
+     * Its Trivial that this sequence will contain only a single element,i.e. value of that node.
+     */
+    if (ptr -> left == NULL && ptr -> right == NULL) {
+        vector<int> seq;
+        seq.push_back(ptr->data);
+        vector<vector<int> > v;
+        v.push_back(seq);
+        return v;
+    }
 
-	}
+    /*
+     * 	Divide Part (this part is very simple.)
+		We assume that we have a function that can solve this problem, and thus we solve it for left sub tree and right sub tree.
+
+		vector<vector<int> > leftSeq  = findAllSeq(ptr -> left);
+		vector<vector<int> > rightSeq = findAllSeq(ptr -> right);
+
+		Merging the two solution.(The crux is in this step.)
+		Till now we have two set containing distinct sequences:
+		i. leftSeq  - all sequences in this set will generate left subtree.
+		ii. rightSeq - all sequences in this set will generate right subtree.
+		Now each sequence in left subtree can be merged with each sequence of right subtree. While merging we should be careful that the
+		relative order of elements is preserved. Also in each of the merged sequence we will add the value of current node in the
+		beginning because root must come before all children.
+     *
+     */
+    vector<vector<int> > results, left, right;
+    left  = findAllSeq(ptr -> left);
+    right = findAllSeq(ptr -> right);
+    int size = left[0].size() + right[0].size() + 1;
+
+    vector<bool> flags(left[0].size(), 0);
+    for (int k = 0; k < right[0].size(); k++)
+        flags.push_back(1);
+
+    for (int i = 0; i < left.size(); i++) {
+        for (int j = 0; j < right.size(); j++) {
+            do {
+                vector<int> tmp(size);
+                tmp[0] = ptr -> data;
+                int l = 0, r = 0;
+                for (int k = 0; k < flags.size(); k++) {
+                    tmp[k+1] = (flags[k]) ? right[j][r++] : left[i][l++];
+                }
+                results.push_back(tmp);
+            } while (next_permutation(flags.begin(), flags.end()));
+        }
+    }
+
+    return results;
 }
 
 int main(){
@@ -108,8 +230,20 @@ int main(){
      //     cout<<p<<endl;
     }
 
-    printInorder(p);
+    vector<vector<int> > vec = findAllSeq(p);
 
+    /*
+     * print result
+     */
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec[i].size(); j++)
+        {
+            cout << vec[i][j] << " ";
+        }
+        cout<<endl;
+    }
 	return 0;
 
 }
